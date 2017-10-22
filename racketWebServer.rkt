a1b4ae250972cf0de75e27f38c0878a1c89d1dd2
Eduardo Ugolini <edu.ugolini2@gmail.com>
2017-10-22 15:29:14 -0200

    Adicionado form com 10 perguntas carregadas do banco

--- racketWebServer.rkt
+++ racketWebServer.rkt
@@ -1,6 +1,8 @@
 #lang racket/base
 
 (require db
+         racket/sequence
+         racket/list
          web-server/servlet
          web-server/servlet-env)
 
@@ -14,8 +16,8 @@
 )
 
 (define questions
-  (query-exec mydb
-              "SELECT * FROM pergunta"
+  (in-query mydb
+              "SELECT pergunta.pergunta FROM pergunta"
   )
 )
 
@@ -26,10 +28,48 @@
      (head (title "Trabalho 1!"))
      (body ([style "background-color: #ccc;"])
                    (form ([action "/validate"])
-                         "Enter the number to add "
+                         
+                         (p "1: ", (first (sequence->list questions)))
                          (br)
-                         (input ([type "text"] [class "1"] [name "number"] [value ""]))
+                         (input ([type "text"] [name "1"] [value ""] [required "true"]))
                          (br)
+                         (p "2: ", (second (sequence->list questions)))
+                         (br)
+                         (input ([type "text"] [name "2"] [value ""] [required "true"]))
+                         (br)
+                         (p "3: ", (third (sequence->list questions)))
+                         (br)
+                         (input ([type "text"] [name "3"] [value ""] [required "true"]))
+                         (br)
+                         (p "4: ", (fourth (sequence->list questions)))
+                         (br)
+                         (input ([type "text"] [name "4"] [value ""] [required "true"]))
+                         (br)
+                         (p "5: ", (fifth (sequence->list questions)))
+                         (br)
+                         (input ([type "text"] [name "5"] [value ""] [required "true"]))
+                         (br)
+                         (p "6: ", (sixth (sequence->list questions)))
+                         (br)
+                         (input ([type "text"] [name "6"] [value ""] [required "true"]))
+                         (br)
+                         (p "7: ", (seventh (sequence->list questions)))
+                         (br)
+                         (input ([type "text"] [name "7"] [value ""] [required "true"]))
+                         (br)
+                         (p "8: ", (eighth (sequence->list questions)))
+                         (br)
+                         (input ([type "text"] [name "8"] [value ""] [required "true"]))
+                         (br)
+                         (p "9: ", (ninth (sequence->list questions)))
+                         (br)
+                         (input ([type "text"] [name "9"] [value ""] [required "true"]))
+                         (br)
+                         (p "10: ", (tenth (sequence->list questions)))
+                         (br)
+                         (input ([type "text"] [name "10"] [value ""] [required "true"]))
+                         (br)
+                         
                          (input ([type "submit"] [name "enter"] [value "Enviar"]))))
     )
   )
